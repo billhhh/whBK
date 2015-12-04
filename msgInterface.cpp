021a30702505aa1280c0245b113dce9091e0f233
Bill Dufresne <371570273@qq.com>
2015-12-04 12:03:12 +0800

    1

--- msgInterface.cpp
+++ msgInterface.cpp
@@ -276,7 +276,7 @@ int msgInterface::moveModuleForIf(int cur_m_id,int other_m_id,
 					MoveType move_type,int container_id,int ui_branch_id
 					) {
 
-	if( container_id == ui_branch_id ) //
+	if( container_id == ui_branch_id ) //目的地是for模块
 		return ;
 }
 
