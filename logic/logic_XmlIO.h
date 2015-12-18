346b281589be8278084d11081cc7fe87c4e95ed5
Bill Dufresne <371570273@qq.com>
2015-12-18 13:52:38 +0800

    1

--- logic/logic_XmlIO.h
+++ logic/logic_XmlIO.h
@@ -8,12 +8,17 @@
 #define _XML_IO_H_
 
 #include <string>
+#include <map>
 #include "logic_Project.h"
+#include "logic_BasicModule.h"
 
 class logic_XmlIO
 {
 public:
-	bool FillPrj(std::string path,logic_Project &prj); //读取xml，填充满一个prj
+	std::map <int, logic_BasicModule *> initAllModule(); //初始化模块
+
+	int SavePrj(std::string path,logic_Project prj); //写入xml
+	int FillPrj(std::string path,logic_Project &prj); //读取xml，填充满一个prj
 
 private:
 	
