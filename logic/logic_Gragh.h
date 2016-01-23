fee713b8531dcc9d647df744f798331b047b4d63
Bill Dufresne <371570273@qq.com>
2016-01-23 18:31:33 +0800

    1

---
+++ logic/logic_Gragh.h
@@ -0,0 +1,32 @@
+/* 
+* 
+* function: “∫æß∆¡ÕºœÒ¿‡
+* 
+* Date:2016-01-23
+* 
+*    Author: Bill Wang
+*/
+
+#ifndef _LOGIC_GRAGH_H_
+#define _LOGIC_GRAGH_H_
+
+#define GRAGH_LENGTH 180
+#define GRAGH_WIDTH 128
+
+class logic_Gragh
+{
+public:
+	int gragh[GRAGH_LENGTH][GRAGH_WIDTH];
+
+	int** getGragh();
+	void setGragh(int ** tmpGragh);
+
+protected:
+
+
+private:
+
+
+};
+
+#endif
