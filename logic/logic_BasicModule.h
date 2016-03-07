8050378896b510f2a095ba82917febbc515a5f00
Bill Dufresne <371570273@qq.com>
2016-03-07 15:56:19 +0800

    1

--- logic/logic_BasicModule.h
+++ logic/logic_BasicModule.h
@@ -24,9 +24,9 @@ public:
 	/////构造函数和析构函数
 	explicit logic_BasicModule();//默认生成start
 	explicit logic_BasicModule(int id);//默认生成start
-	explicit logic_BasicModule(int id,int moduleType); //根据信息，自动填充其他信息
+	logic_BasicModule(int id,int moduleType); //根据信息，自动填充其他信息
 	////深拷贝构造函数
-	explicit logic_BasicModule(int id,int moduleType,const logic_BasicModule &bm);
+	logic_BasicModule(int id,int moduleType,const logic_BasicModule &bm);
 	virtual ~logic_BasicModule();
 
 	/////!!! useless !!!!!!!!!!!!
--- logic/logic_MyBlockModule.cpp
+++ logic/logic_MyBlockModule.cpp
@@ -9,20 +9,24 @@
 
 #include "logic_MyBlocksModule.h"
 
-//构造函数
+///
+/// \brief 构造函数+析构函数
+///
 logic_MyBlocksModule::logic_MyBlocksModule()
 	:mvi_MyBlockProgID(0),mvi_MyBlockProg(NULL) {
 
 }
 
-//构造函数
 logic_MyBlocksModule::logic_MyBlocksModule(int mbprog_id)
 	:mvi_MyBlockProgID(mbprog_id),mvi_MyBlockProg(NULL) {
 
 }
 
-//构造函数
 logic_MyBlocksModule::logic_MyBlocksModule(logic_MyBlocksProgram *mbprog)
 	:mvi_MyBlockProgID(mbprog->getID()),mvi_MyBlockProg(mbprog) {
+
+}
+
+logic_MyBlocksModule::~logic_MyBlocksModule() {
 
 }
